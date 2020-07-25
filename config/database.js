3c85c91d751828931a471a0e30b3274197016a0e
Annie Taylor CHEN <annietaylorchen@gmail.com>
2020-07-25 10:50:14 +0200

    Configured production/database.json

--- config/database.js
+++ config/database.js
@@ -1,14 +1,18 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        username: env("DATABASE_USERNAME", ""),
+        password: env("DATABASE_PASSWORD", ""),
       },
       options: {
-        useNullAsDefault: true,
+        ssl: false,
       },
     },
   },
